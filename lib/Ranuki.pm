f7ba475b74137aa1ca5596a5478f1b6201f5bb9d
Sota Sugiura <t11460ss@sfc.keio.ac.jp>
2015-01-18 20:25:58 +0900

    コンストラクタ書いた

--- lib/Ranuki.pm
+++ lib/Ranuki.pm
@@ -1,3 +1,19 @@
 package Ranuki;
 
+use utf8;
+use Encode;
 
+binmode(STDOUT, ":encoding(utf-8)");
+
+# Constructor
+sub new {
+  my ($ck, $csk, $at, $ats) = @_;
+
+  my $nt = Net::Twitter->new(
+    consumer_key        => $ck,
+    consumer_secret     => $csk,
+    access_token        => $at,
+    access_token_secret => $ats,
+    ssl => 1
+  );
+}
